3596cd61a861cf13c9743f5b7b1d1517559867aa
greffgreff <greffchandler80@gmail.com>
2023-02-28 22:54:42 +0100

    Worked on sidebar

--- src/view/components/Sidebar.tsx
+++ src/view/components/Sidebar.tsx
@@ -3,9 +3,12 @@ import "../styles/Sidebar.css";
 export default function Sidebar() {
   return (
     <div className="sidebar-wrapper">
-      <div onClick={() => window.electron.logout()}>
-        <div className="profile" />
-      </div>
+      <div className="profile" onClick={() => window.electron.logout()} />
+      <div className="sidebar-item-separator" />
+      <div className="profile add-friend" />
+      <div className="profile" />
+      <div className="sidebar-item-separator" />
+      <div className="profile" />
       <div className="profile" />
       <div className="profile" />
       <div className="profile" />
