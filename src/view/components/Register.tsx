19ab372c3263ed168c994ab508766c2cf2fd9b71
greffgreff <greffchandler80@gmail.com>
2023-02-28 07:03:53 +0100

    Fixed typo

--- src/view/components/Register.tsx
+++ src/view/components/Register.tsx
@@ -23,17 +23,15 @@ export default function Register() {
         <span>Looks like this is the first time you are running this app. Register a user and begin chating!</span>
       </div>
       <form className="registration-form" onSubmit={handleSubmit(onClick)}>
-        <>
-          {errors.username?.type === "required" && <p className="validation-error">Username is required.</p>}
-          {errors.username?.type === "maxLength" && <p className="validation-error">Username must cannot be longer than 20 characters</p>}
-          <input type="text" placeholder="Enter a username" {...register("username", { required: true, maxLength: 20 })} />
-          {errors.password?.type === "required" && <p className="validation-error">Password is required.</p>}
-          {errors.password?.type === "minLength" && <p className="validation-error">Password must be at least 10 characters long</p>}
-          <input type="text" placeholder="Enter a password" {...register("password", { required: true, minLength: 10 })} />
-          <input type="submit" value="Register" />
-        </>
+        {errors.username?.type === "required" && <p className="validation-error">Username is required.</p>}
+        {errors.username?.type === "maxLength" && <p className="validation-error">Username must cannot be longer than 20 characters</p>}
+        <input type="text" placeholder="Enter a username" {...register("username", { required: true, maxLength: 20 })} />
+        {errors.password?.type === "required" && <p className="validation-error">Password is required.</p>}
+        {errors.password?.type === "minLength" && <p className="validation-error">Password must be at least 10 characters long</p>}
+        <input type="text" placeholder="Enter a password" {...register("password", { required: true, minLength: 10 })} />
+        <input type="submit" value="Register" />
       </form>
-      <div className="registration-note">Please note that a user is tied to a device. Upon registration, your credentials will only be valid this device.</div>
+      <div className="registration-note">Please note that a user is tied to a device. Upon registration, your credentials will only be valid on this device.</div>
     </div>
   );
 }
