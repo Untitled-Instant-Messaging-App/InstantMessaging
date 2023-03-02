b9c115f51c5834ef9c1eef2bb7261249e2714e52
chandle greff <unconfigured@null.spigotmc.org>
2023-03-02 05:04:51 +0100

    Added welcome page

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -6,10 +6,13 @@ import useAuth from "./hooks/useAuth";
 import Sidebar from "./components/Sidebar";
 import Chat from "./components/Chat";
 import { useState } from "react";
+import Safe from "./assets/Safe";
+import Rocket from "./assets/Rocket";
+import Phone from "./assets/Phone";
 
 export default function App() {
   const { state, isAuthed } = useAuth();
-  const [selectedChat, setSelectedChat] = useState("Initial state");
+  const [selectedChat, setSelectedChat] = useState(null);
 
   // if (state === AuthState.NotRegistered || state === AuthState.Registering) {
   //   return <Register state={state} />;
@@ -23,7 +26,27 @@ export default function App() {
     // isAuthed &&
     <div className="app-wrapper">
       <Sidebar onClick={setSelectedChat} />
-      <Chat chat={selectedChat} />
+      {selectedChat ? (
+        <Chat chat={selectedChat} />
+      ) : (
+        <div className="hello-there">
+          <div>
+            <h1>Secure</h1>
+            <Safe />
+            <p>E2E encrypted messages using the Signal Protocol</p>
+          </div>
+          <div>
+            <h1>Fast</h1>
+            <Rocket />
+            <p>Decentralized XMP communication</p>
+          </div>
+          <div>
+            <h1>Intuitive</h1>
+            <Phone />
+            <p>Familar design language we all expect</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
