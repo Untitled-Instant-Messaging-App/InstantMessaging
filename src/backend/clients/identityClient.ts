c6fd6d1dfd32c6202beaffd17d59d0096a2805ec
greffgreff <greffchandler80@gmail.com>
2023-03-08 11:14:37 +0100

    Removed env

--- src/backend/clients/identityClient.ts
+++ src/backend/clients/identityClient.ts
@@ -1,10 +1,17 @@
 import axios from "axios";
 import { BasicUser, User } from "../../common/types";
+import https from "https";
+import isDev from "electron-is-dev";
 
 export async function register(user: BasicUser): Promise<User> {
-  return await axios.post("https://localhost:44354" + "/users/register", user);
+  const res = await axios.post("https://localhost:44354" + "/users/register", user, headers());
+  return res.data;
 }
 
 export async function unregister(user: User): Promise<void> {
   await axios.delete("https://localhost:44354" + "/users/unregister/" + user.id);
+}
+
+function headers() {
+  return isDev ? { httpsAgent: new https.Agent({ rejectUnauthorized: false }) } : null;
 }
--- src/backend/main.ts
+++ src/backend/main.ts
@@ -6,8 +6,6 @@ import isDev from "electron-is-dev";
 
 require("electron-squirrel-startup") && app.quit();
 
-require('dotenv').config();
-
 declare const MAIN_WINDOW_WEBPACK_ENTRY: string;
 declare const MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY: string;
 
